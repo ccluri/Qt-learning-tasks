a87ca3db2120068f31858dad8ed322b520fc3624
Chaitanya <ccluri@gmail.com>
2011-01-04 22:54:21 +0530

    Changed starting header credits.

--- Globe&Satellite v1.0/glwidget.h
+++ Globe&Satellite v1.0/glwidget.h
@@ -1,10 +1,3 @@
-/*
- * This code was created by Jeff Molofee 1999
- * And ported to C++/Qt4 by Wesley Stessens 2009
- *
- * Contact Jeff through his website: http://nehe.gamedev.net/
- * Contact Wesley for port-specific comments: wesley@ubuntu.com
- */
 
 #ifndef GLWIDGET_H
 #define GLWIDGET_H
